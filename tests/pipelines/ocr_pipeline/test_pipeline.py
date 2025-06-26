29affddef8c429e9e94f59ec7c1a5f8ebae7b3e9
Mouhamed SANKHANE <mouhamed-moustapha.sankhane@bedrockstreaming.com>
2025-06-26 10:59:30 +0200

    fix: upgrade test cov

--- tests/pipelines/ocr_pipeline/test_pipeline.py
+++ tests/pipelines/ocr_pipeline/test_pipeline.py
@@ -1,9 +1,33 @@
-"""
-This is a boilerplate test file for pipeline 'ocr_pipeline'
-generated using Kedro 0.19.12.
-Please add your pipeline tests here.
+import unittest
+from unittest.mock import patch, MagicMock
+import os
 
-Kedro recommends using `pytest` framework, more info about it can be found
-in the official documentation:
-https://docs.pytest.org/en/latest/getting-started.html
-"""
+# On importe la fonction à tester
+from  src.kedro_road_sign.pipelines.ocr_pipeline.nodes import run_ocr_on_detection
+
+class TestRunOcrOnDetection(unittest.TestCase):
+
+    @patch("os.listdir")
+    @patch("cv2.imread")
+    @patch("pytesseract.image_to_string")
+    def test_run_ocr_on_detection(self, mock_ocr, mock_imread, mock_listdir):
+        # Données de test
+        mock_listdir.return_value = ["image1.jpg", "image2.png", "not_image.txt"]
+        mock_imread.return_value = MagicMock()  # un faux objet image
+        mock_ocr.side_effect = ["Text from image1", "Text from image2"]
+
+        folder_path = "/fake/path"
+        expected_result = {
+            "image1.jpg": "Text from image1",
+            "image2.png": "Text from image2"
+        }
+
+        result = run_ocr_on_detection(folder_path)
+        self.assertEqual(result, expected_result)
+
+        mock_listdir.assert_called_once_with(folder_path)
+        self.assertEqual(mock_ocr.call_count, 2)
+        self.assertEqual(mock_imread.call_count, 2)
+
+if __name__ == "__main__":
+    unittest.main()
