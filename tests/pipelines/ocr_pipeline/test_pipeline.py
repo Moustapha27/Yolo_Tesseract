fc57524dc854a311eb5d827174792afc5ca80330
Mouhamed SANKHANE <mouhamed-moustapha.sankhane@bedrockstreaming.com>
2025-06-26 12:14:55 +0200

    upgrade tests

--- tests/pipelines/ocr_pipeline/test_pipeline.py
+++ tests/pipelines/ocr_pipeline/test_pipeline.py
@@ -9,21 +9,27 @@ from src.kedro_road_sign.pipelines.ocr_pipeline.pipeline import create_pipeline
 
 
 @pytest.fixture
-def sample_image(tmp_path):
+def sample_images(tmp_path):
     # Crée une image temporaire avec du texte
     image = np.ones((100, 300), dtype=np.uint8) * 255  # fond blanc
     cv2.putText(image, 'Hello', (5, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (0), 3)
-    image_path = tmp_path / "test_ocr.png"
-    cv2.imwrite(str(image_path), image)
+    
+    png_path = tmp_path / "test_ocr.png"
+    jpg_path = tmp_path / "test_ocr.jpg"
+    
+    cv2.imwrite(str(png_path), image)
+    cv2.imwrite(str(jpg_path), image)
+    
     return tmp_path
 
-def test_run_ocr_on_detection(sample_image):
-    results = run_ocr_on_detection(str(sample_image))
+def test_run_ocr_on_detection(sample_images):
+    results = run_ocr_on_detection(str(sample_images))
     assert "test_ocr.png" in results
+    assert "test_ocr.jpg" in results
     assert isinstance(results["test_ocr.png"], str)
+    assert isinstance(results["test_ocr.jpg"], str)
     assert "hello" in results["test_ocr.png"].lower()
-
-
+    assert "hello" in results["test_ocr.jpg"].lower()
 
 def test_pipeline_structure():
     pipeline = create_pipeline()
