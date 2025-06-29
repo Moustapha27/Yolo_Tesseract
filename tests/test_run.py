d2ae2d7d8a23b44f8e11a52d4a57d351898280bf
Mouhamed SANKHANE <mouhamed-moustapha.sankhane@bedrockstreaming.com>
2025-06-30 00:13:54 +0200

    modification du test

--- tests/test_run.py
+++ tests/test_run.py
@@ -3,14 +3,13 @@ from pathlib import Path
 from kedro.framework.session import KedroSession
 from kedro.framework.startup import bootstrap_project
 
-
 class TestKedroRun:
     @patch("src.kedro_road_sign.pipelines.ocr_pipeline.nodes.pytesseract.get_tesseract_version")
-    @patch("torch.load")
-    def test_kedro_run(self, mock_torch_load, mock_get_version):
+    def test_kedro_run(self, mock_get_version):
+        # Simuler que Tesseract est bien installé en retournant une version fictive
         mock_get_version.return_value = "5.3.0"
-        mock_torch_load.return_value = "mocked_model_object"  # ou un objet factice
-        
+
         bootstrap_project(Path.cwd())
+
         with KedroSession.create(project_path=Path.cwd()) as session:
             assert session.run() is not None
